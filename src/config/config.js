ebdbd6ca5bc8ed284eb5a100577b489ddf42006e
Matías <matiasnicolaslazarte@gmail.com>
2026-02-16 00:27:59 -0300

    MB-6 feat(config): Agrega Se agregan: mongoUri, jwtSecret, jwtExpiresIn

--- src/config/config.js
+++ src/config/config.js
@@ -2,6 +2,9 @@ require('dotenv').config();
 
 module.exports = {
     port: process.env.port || 5000,
+    mongoUri: process.env.MONGO_URI,
+    jwtSecret: process.env.JWT_SECRET,
+    jwtExpiresIn: process.env.JWT_EXPIRES_IN || '7d',
     nodeEnv: process.env.NODE_ENV || 'development',
     frontendUrl: process.env.FRONTEND_URL || 'http://localhost:3000'
 };
