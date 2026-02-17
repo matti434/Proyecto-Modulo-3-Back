80eba2b3f2a1c4b11f962ddd7514525fdbaa59fd
MatiasLazarte <matiasnicolaslazarte@gmail.com>
2026-02-17 14:27:45 -0300

    MB-14 feat: controlador de productos con listado, filtro y creacion

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -1,16 +1,41 @@
-const products = [
-  { id: 1, name: 'Producto 1', price: 100 },
-  { id: 2, name: 'Producto 2', price: 200 }
-];
+const { Producto } = require('../Models');
 
-const getProducts = (req, res, next) => {
+const getProducts = async (req, res, next) => {
   try {
+    const { categoria, destacado, limite } = req.query;
+    const query = {};
+    if (categoria) query.categoria = new RegExp(categoria, 'i');
+    if (destacado === 'true') query.destacado = true;
+
+    let builder = Producto.find(query);
+    if (limite) builder = builder.limit(Number(limite));
+    const products = await builder.lean();
+
     res.json(products);
   } catch (error) {
     next(error);
   }
 };
 
-module.exports = {
-  getProducts
+const getProductById = async (req, res, next) => {
+  try {
+    const product = await Producto.findById(req.params.id).lean();
+    if (!product) {
+      const err = new Error('Producto no encontrado');
+      err.statusCode = 404;
+      return next(err);
+    }
+    res.json(product);
+  } catch (error) {
+    next(error);
+  }
+};
+
+const createProduct = async (req, res, next) => {
+  try {
+    const product = await Producto.create(req.body);
+    res.status(201).json(product);
+  } catch (error) {
+    next(error);
+  }
 };
