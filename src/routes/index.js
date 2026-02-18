f2302456f2afa255be0c6540238b4a7907a8c875
Matías <matiasnicolaslazarte@gmail.com>
2026-02-17 21:37:32 -0300

    chore: corrige errores de sintaxis

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-const authRoutes = require('./authRoutes');
 const usuarioRoutes = require('./usuariosRoutes');
 const productoRoutes = require('./productoRoutes')
 const pedidoRoutes = require('./pedidoRoutes');
