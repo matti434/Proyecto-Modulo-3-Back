d545cbf18938b9aa7be232b7c15a7efddf060fc2
RominaDanelutto <r.danelutto@gmail.com>
2026-03-18 00:37:42 -0300

    MB-29 feat: agrega campos base usuario, items, subtotal y envio

--- src/models/Pedido.js
+++ src/models/Pedido.js
@@ -17,7 +17,24 @@ const pedidoItemSchema = new mongoose.Schema({
   }
 }, { _id: true });
 
-const pedidoSchema = new mongoose.Schema({}, {
+const pedidoSchema = new mongoose.Schema({
+  usuario: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Usuario',
+    required: true
+  },
+  items: [pedidoItemSchema],
+  subtotal: {
+    type: Number,
+    required: true,
+    min: 0
+  },
+  envio: {
+    type: Number,
+    default: 0,
+    min: 0
+  }
+}, {
   timestamps: true
 });
 
